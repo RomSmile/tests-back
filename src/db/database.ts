967319c985acbfd6c63a2c462e98360dcb1ca1b9
Rom_smile <ryacenko3@gmail.com>
2023-11-27 18:06:37 +0200

    updated db settings

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,11 +2,11 @@ import { Pool } from 'pg';
 require('dotenv-safe').config();
 
 const pool: typeof Pool = new Pool({
-    user: 'postgres',
-    password: '10092003',
-    host: 'localhost',
-    port: 5432,
-    database: 'exercises'
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_URL,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME
 })
 
 export default pool;
