223ccb0e0c622c50344b1b866296aea9bd90699e
Roman <86207804+RomSmile@users.noreply.github.com>
2024-01-23 17:34:16 +0200

    Update schemas.ts

--- src/controllers/exercise-contoller/schemas.ts
+++ src/controllers/exercise-contoller/schemas.ts
@@ -12,7 +12,6 @@ export const exerciseCreateSchema = Joi.object({
 })
 
 export const answerCheckExerciseSchema = Joi.array().items({
-  id: Joi.string().min(36).max(36).required(),
   isSelected: Joi.boolean().valid(true).required(),
   questionId: Joi.string().min(36).max(36).required(),
   exerciseId: Joi.string().min(36).max(36).required(),
