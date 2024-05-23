7a5b1a88d863317e29f3927cec78c56356e2ab0a
RomSmile <ryacenko3@gmail.com>
2024-05-23 17:09:37 +0300

    update validation

--- src/controllers/exercise-contoller/schemas.ts
+++ src/controllers/exercise-contoller/schemas.ts
@@ -5,7 +5,7 @@ export const exerciseCreateSchema = Joi.object({
   questions: Joi.array().items({
     title: Joi.string().max(300).min(3).required(),
     answers: Joi.array().items({
-      text: Joi.string().min(3).required(),
+      text: Joi.string().min(1).required(),
       isCorrectAnswer: Joi.boolean().required(),
     }).has(Joi.object({ text: Joi.string(), isCorrectAnswer: Joi.boolean().valid(true)})).min(2).required()
   }).min(2).required(),
